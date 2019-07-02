55286d248cc1b788ef4f88fd8d28376c9c7b4b7f
TakyMalika <e1801588@edu.vamk.fi>
2019-07-02 19:06:49 +0300

    Calculating the ingredient sun dynamically (important)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,13 +12,14 @@ class BurgerBuilder extends Component {
     state = {
         ingredients: {
             salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
+            bacon: 0,
+            cheese: 0,
+            meat: 0
         }
     }
-
+    
     render () {
+        // console.log(this.state);
         return (
             <Aux>
                 <Burger ingredients= {this.state.ingredients} />
